f1db7dd6520019d41e84782eaefbea56a9253f09
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2025-02-11 23:54:18 +0200

    Complete exercise 12.16

--- todo-app/todo-backend/dev.Dockerfile
+++ todo-app/todo-backend/dev.Dockerfile
@@ -1,7 +1,7 @@
 FROM node:20
 WORKDIR /usr/src/app
-COPY --chown=node:node . . 
-RUN npm ci
+COPY . .
+RUN npm install
 ENV DEBUG=todo-express-backend:*
-USER node
-CMD npm run start
+CMD npm run dev
+
--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -7,5 +7,5 @@ console.log(REDIS_URL);
 
 module.exports = {
   MONGO_URL, //: 'mongodb://the_username:the_password@localhost:3456/the_database',
-  REDIS_URL, //: '//localhost:6378'
+  REDIS_URL, //: 'redis://localhost:6378'
 };
